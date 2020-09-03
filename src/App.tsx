aa23444236d9ef2b09704935a5a650839f459f89
inmax <inmaculadaminarroruiz@gmail.com>
2020-09-04 00:16:04 +0200

    init project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+//import Home from "./pages/Home";
+// import Layout from "./components/Layout";
+// import ContentTop from "./components/ContentTop";
+// import "./app.scss";
+
+
+export default function App() {
+  return (
+    <>
+      {/* <Layout
+        header={<ContentTop />}
+        content={<Home />}
+      /> */}
+      <h1>eeeeee</h1>
+    </>)
+}
---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// import { Provider } from "react-redux";
+import 'normalize.css';
+// import './theme/main.scss';
+
+ReactDOM.render(<App />,
+    document.getElementById("output")
+);
