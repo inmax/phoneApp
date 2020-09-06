e25a5208530bcaa60ff460793100780bf151e29d
inmax <inmaculadaminarroruiz@gmail.com>
2020-09-06 13:35:34 +0200

    style components phone item

--- src/components/PhoneItem/index.tsx
+++ src/components/PhoneItem/index.tsx
@@ -6,22 +6,45 @@ interface PropsPhoneItem {
   phone: PhoneModel;
   onShow?: () => void;
 }
+const defaultPropsPhoneModel = {
+  name: 'Name',
+  screen: 'Pantalla',
+  price: '0',
+  imageFileName: 'asset',
+  processor: 'Processor',
+};
 
 export default function PhoneItem({ phone, onShow }: PropsPhoneItem): JSX.Element {
-  const imgage = phone.imageFileName;
+  const {
+    name = defaultPropsPhoneModel.name,
+    screen = defaultPropsPhoneModel.name,
+    price = defaultPropsPhoneModel.price,
+    imageFileName = defaultPropsPhoneModel.imageFileName,
+    processor = defaultPropsPhoneModel.imageFileName,
+  } = phone;
+
   return (
-    <article className="item-product">
+    <article className="item-product" onClick={onShow}>
       <header>
-        <h4 className="item-product__title">{phone.name}</h4>
-        <p>Preprocessor: {phone.processor} </p>
+        <h4 className="item-product__title">{name}</h4>
+        <p className="item-product__excerpt">
+          Preprocessor: {processor}
+          <br></br>
+          screen: {screen}{' '}
+        </p>
       </header>
-      <figure>
+      <figure className="item-product__figure">
         <img
-          src={`https://raw.githubusercontent.com/inmax/interview-code-challenges/master/react/phone-catalogue/images/${imgage}`}
+          className="item-product__image"
+          src={`https://raw.githubusercontent.com/inmax/interview-code-challenges/master/react/phone-catalogue/images/${imageFileName}`}
+          alt={`Imagen del modelo ${name}`}
         />
       </figure>
       <footer className="item-product__footer">
-        <p>{phone.price}€ </p>
+        <p>
+          {price}
+          <sup>€</sup>{' '}
+        </p>
       </footer>
     </article>
   );
